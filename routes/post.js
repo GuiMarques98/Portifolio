1337f578bfa9d6f14bb9536ac1c62cebcee479a6
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2020-09-29 21:07:08 -0300

    adding post get by ip

--- routes/post.js
+++ routes/post.js
@@ -2,12 +2,29 @@ const express = require('express')
 const router = express.Router()
 const Post = require('../models/Post')
 
-router.get('/', (req, res) => {
-    res.send('We are on post')
+router.get('/', async (req, res) => {
+    try {
+        const post = await Post.find()
+        res.json(post)
+    } catch (err) {
+        res.json(err)
+    }
 })
+
+router.get('/:postId', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId)
+        res.json(post)
+    } catch (err) {
+        res.json(err)
+    }
+})
+
 
 router.post('/', async (req, res) => {
     const post = new Post({ title: req.body.title, description: req.body.description })
+    console.log(req.body)
+    console.log(post)
     try {
         const savePost = post.save()
         res.json(savePost)
